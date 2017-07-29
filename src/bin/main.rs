7dffccaf875478f537654d1cdc535580f11e2d24
Matthew Wynn <matthew@matthewwynn.com>
2017-07-29 18:47:52 -0500

    Parse config file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,29 @@
 extern crate casm;
 
-use casm::Config;
+use casm::errors::Error;
 use std::process;
 
 fn main() {
-    let config = Config::new().unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    if let Err(e) = casm::run(config) {
-        println!("Application error: {}", e);
-
-        process::exit(1);
+    if let Err(ref err) = casm::run() {
+        handle_error(err);
     }
+}
+
+fn handle_error(e: & Error ) {
+    use std::io::Write;
+    let stderr = &mut ::std::io::stderr();
+    let errmsg = "Error writing to stderr";
+    writeln!(stderr, "error: {}", e).expect(errmsg);
+
+    for e in e.iter().skip(1) {
+        writeln!(stderr, "caused by: {}", e).expect(errmsg);
+    }
+
+    // The backtrace is not always generated. Try to run this example
+    // with `RUST_BACKTRACE=1`.
+    if let Some(backtrace) = e.backtrace() {
+        writeln!(stderr, "backtrace: {:?}", backtrace).expect(errmsg);
+    }
+
+    ::process::exit(1);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,66 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate error_chain;
+extern crate toml;
+extern crate xdg;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 
 use clap::App;
-use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::PathBuf;
 
-pub struct Config;
+pub mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain!{}
+}
+
+pub use errors::*;
+
+#[derive(Deserialize)]
+pub struct Config {
+    source_folder: String,
+}
 
 impl Config {
-    pub fn new() -> Result<Config, &'static str> {
+    pub fn new() -> Result<Config> {
         let yaml = load_yaml!("cli.yml");
         let matches = App::from_yaml(yaml)
             .author(crate_authors!())
             .version(crate_version!())
             .get_matches();
-        Ok(Config{})
+        let config_file = match matches.value_of("config") {
+            Some(file) => { PathBuf::from(file) },
+            _ => {
+                let xdg_dirs = xdg::BaseDirectories::with_prefix("casm")
+                    .chain_err(|| "Unable to get XDG directories")?;
+                if let Some(file) = xdg_dirs.find_config_file("config.toml") {
+                    file
+                } else {
+                    bail!("Could not load config.toml")
+                }
+            },
+        };
+
+        let mut config_file = File::open(config_file)
+            .chain_err(|| "Unable to open config file")?;
+        let mut contents = String::new();
+        config_file.read_to_string(&mut contents)
+            .chain_err(|| "Unable to read config file")?;
+
+        // TODO: Allow overriding by CLI args
+        let config = toml::from_str(contents.as_str())
+            .chain_err(|| "Could not parse config file")?;
+
+        Ok(config)
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<Error>> {
+pub fn run() -> Result<()> {
+    let config = Config::new()
+        .chain_err(|| "Unable to read config")?;
     Ok(())
 }
